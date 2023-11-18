3597b6d61aca1509b91838bf685a990a28a0981b
Hayat Mahmud <amasr59@gmail.com>
2023-11-18 02:58:59 -0500

    Student feature done

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,22 @@
-import express, { Application, Request, Response } from 'express'
-import cors from 'cors'
-const app: Application = express()
+import cors from 'cors';
+import express, { Application, Request, Response } from 'express';
+import { StudentRoutes } from './app/config/modules/student/student.route';
 
+const app: Application = express();
 
 //parsers
-app.use(express.json())
-app.use(cors);
+app.use(express.json());
+app.use(cors());
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!')
-})
+// application routes
+app.use('/api/v1/students', StudentRoutes);
+
+const getAController = (req: Request, res: Response) => {
+  const a = 10;
+
+  res.send(a);
+};
+
+app.get('/', getAController);
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,17 @@
-import config from "./app/config";
-import app from "./app";
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
+import app from './app';
+import config from './app/config';
 
 async function main() {
-  try{
+  try {
     await mongoose.connect(config.database_url as string);
 
     app.listen(config.port, () => {
-      console.log(`Example app listening on port ${config.port}`)
-    })
-  }catch(err){
-    console.log(err)
+      console.log(`App is listening in port ${config.port}`);
+    });
+  } catch (err) {
+    console.log(err);
   }
+}
 
-    
-  }
-
-  main();
-
+main();
