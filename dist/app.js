15bc027d940b9fd30336b8b2493558816a53fc28
Hayat Mahmud <amasr59@gmail.com>
2023-11-21 09:27:22 -0500

    done create and get a student from database

--- dist/app.js
+++ dist/app.js
@@ -1,10 +1,19 @@
 "use strict";
-const express = require("express");
-const app = express();
-const port = 3000;
-app.get("/", (req, res) => {
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const cors_1 = __importDefault(require("cors"));
+const express_1 = __importDefault(require("express"));
+const student_route_1 = require("./app/modules/student/student.route");
+const app = (0, express_1.default)();
+//parsers
+app.use(express_1.default.json());
+app.use((0, cors_1.default)());
+//application route
+app.use("/api/v1/students", student_route_1.studentRoutes);
+const getAController = (req, res) => {
     res.send("Hello World!");
-});
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-});
+};
+app.get("/", getAController);
+exports.default = app;
--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,19 @@
 import cors from "cors";
 import express, { Application, Request, Response } from "express";
+import { studentRoutes } from "./app/modules/student/student.route";
 const app: Application = express();
 
 //parsers
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req: Request, res: Response) => {
+//application route
+app.use("/api/v1/students", studentRoutes);
+
+const getAController = (req: Request, res: Response) => {
   res.send("Hello World!");
-});
+};
+
+app.get("/", getAController);
 
 export default app;
